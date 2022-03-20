4f9a5ca734a419cfa56bc091ba1b2cb65bd05592
aijphy <aijphy@gmail.com>
2022-03-20 12:27:40 -0600

    finished pong

--- 100-days-challenges/pong/main.py
+++ 100-days-challenges/pong/main.py
@@ -1,36 +1,74 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
-print('here1')
+#setup screen
 screen = Screen()
-print('here1.1')
 screen.tracer(0)
-
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
 screen.title("Pong")
-print('here2')
 
+#setup pong components:
 l_paddle = Paddle(-350,0)
 r_paddle = Paddle(350,0)
-
-screen.listen()
-screen.onkey(l_paddle.go_up,"w")
-screen.onkey(l_paddle.go_dn,"s")
-screen.onkey(r_paddle.go_up,"Up")
-screen.onkey(r_paddle.go_dn,"Down")
-
-
 ball = Ball()
+scoreboard = Scoreboard()
+
+# add pausing function:
+paused = False
+def pause():
+    global paused
+    if not paused :
+        paused = True
+    else:
+        paused = False
+
+
+# add event listeners:
+screen.listen()
+screen.onkeypress(l_paddle.go_up,"w")
+screen.onkeypress(l_paddle.go_dn,"s")
+screen.onkeypress(r_paddle.go_up,"Up")
+screen.onkeypress(r_paddle.go_dn,"Down")
+screen.onkeyrelease(l_paddle.stop,"w")
+screen.onkeyrelease(l_paddle.stop,"s")
+screen.onkeyrelease(r_paddle.stop,"Up")
+screen.onkeyrelease(r_paddle.stop,"Down")
+screen.onkeypress(pause," ")
 
 
 game_is_on=True
 while game_is_on:
+    
+    time.sleep(0.05)
     screen.update()
 
-    ball.move()
-    time.sleep(0.1)
+    if not paused:
+        screen.update()
+        ball.move()
+        l_paddle.move()
+        r_paddle.move()
+
+
+    # detect collision with wall
+    if abs(ball.ycor()) > 280 :
+        ball.bounce_y()
+    
+    # detect collision with paddles
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320 and ball.xv > 0) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320 and ball.xv < 0):
+        ball.bounce_x()
+
+    # check if scored
+    if (ball.xcor() > 400):
+        scoreboard.increase_l_score()
+        ball.reset_position()
+
+    if (ball.xcor() < -400):
+        scoreboard.increase_r_score()
+        ball.goto(0,0)
+        ball.reset_position()
 
 screen.exitonclick()
